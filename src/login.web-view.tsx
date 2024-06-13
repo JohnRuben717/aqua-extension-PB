60b19185dec85762112e95d4d831e3587938152d
isaac-joshua <joshuaisaac@karunya.edu.in>
2024-06-13 11:12:43 +0530

    local storage-json file-credentials

--- src/login.web-view.tsx
+++ src/login.web-view.tsx
@@ -1,11 +1,38 @@
+import React, { useState } from 'react';
+
 global.webViewComponent = function FirstWebView() {
-    return (
-        <div style={{ textAlign: 'center' }}>
-          <div>AQuA extention login</div>
-          <input type="text" placeholder="Username" /><br /><br />
-          <input type="password" placeholder="Password" /><br /><br />
-          <button>Submit</button>
-        </div>
-      );
-  }
-  
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = () => {
+    const credentials = {
+      username: username,
+      password: password,
+    };
+    localStorage.setItem('credentials', JSON.stringify(credentials));
+    alert('Credentials saved!');
+  };
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <div>AQuA extension login</div>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <br />
+      <br />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br />
+      <br />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+};
